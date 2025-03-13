07f0ed31ce74f6470519202e4077609ec684b305
ST10445383 <ST10445383@vcconnect.edu.za>
2025-03-13 15:26:29 +0200

    Circle Class

---
+++ Ice_Task_2/Circle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ice_Task_2
+{
+    class Circle : Shape, IShape
+    {
+        public double Radius { get; set; }
+
+        public Circle(string name, double radius) : base(name)
+        {
+            Radius = radius;
+        }
+
+        public double CalculateArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+
+        public override void Display()
+        {
+            base.Display();
+            Console.WriteLine($"Radius: {Radius}, Area: {CalculateArea():F2}");
+        }
+    }
+}
+
--- Ice_Task_2/Program.cs
+++ Ice_Task_2/Program.cs
@@ -66,6 +66,7 @@ namespace Ice_Task_2
             }
         }
     }
+}
 
 
  
